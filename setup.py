5120898ef070c40fc8b4068eee0765fb39b1610d
olofnuggts <yousef_kanzouh@yahoo.com>
2023-12-02 17:53:31 +0100

    Storing data (#1)
    * Add MongoDB logging functionality
    
    * Fix bug in login functionality
    
    * Commented out import json statement
    
    * Remove unused code and import

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SIEM-Simulator",
+    version="0.1.0",  # Update the version number for new releases
+    author="olof nuggets",  # Replace with your name
+    description="A simple SIEM (Security Information and Event Management) data simulator.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/olofnuggts/logie",  # Replace with the URL to your repository
+    packages=find_packages(),
+    install_requires=[
+        "pymongo",  # Add other dependencies as needed
+        "faker",  # Assuming Faker is used for generating log data
+        # Add other dependencies as required by your project
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",  # Minimum version requirement of the python
+)
